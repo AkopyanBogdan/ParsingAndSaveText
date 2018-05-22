e98b77e5e7ff55aaa6f5123a2a0b119647ac9183
Bohdan <akopyan.bogdan@gmail.com>
2018-05-22 15:42:36 +0300

    Create IdentityModel

---
+++ ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Models/IdentityModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ParsingAndSaveTextInDb.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<FileInformation> FileInformation { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+    }
+}
