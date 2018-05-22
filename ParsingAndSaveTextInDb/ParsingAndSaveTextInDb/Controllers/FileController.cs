f113b1a1bc64623fa039cacc46bfbbc7da66484c
Bohdan <akopyan.bogdan@gmail.com>
2018-05-22 15:55:12 +0300

    Create ctor and Logic for Get action

--- ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Controllers/FileController.cs
+++ ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Controllers/FileController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ParsingAndSaveTextInDb.Models;
 
 namespace ParsingAndSaveTextInDb.Controllers
 {
     public class FileController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public FileController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         // GET: File
         public ActionResult Index()
         {
-            return View();
+            var db = _context.FileInformation.ToList();
+            return View(db);
         }
     }
 }
