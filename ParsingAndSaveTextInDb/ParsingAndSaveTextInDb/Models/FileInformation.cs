153eaad64362944e9adfb364fc5b83a497622d51
Bohdan <akopyan.bogdan@gmail.com>
2018-05-22 15:39:41 +0300

    Create Model FileInformation fixes #6

---
+++ ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Models/FileInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParsingAndSaveTextInDb.Models
+{
+    public class FileInformation
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public int Count { get; set; }
+        public byte[] Sentence { get; set; }
+    }
+}
