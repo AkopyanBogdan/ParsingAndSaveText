f85547ccebedc85009067eabd7f69c593eb27a90
Bohdan <akopyan.bogdan@gmail.com>
2018-05-22 16:03:15 +0300

    Create method SearchWord

--- ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Controllers/FileController.cs
+++ ParsingAndSaveTextInDb/ParsingAndSaveTextInDb/Controllers/FileController.cs
@@ -16,6 +16,42 @@ namespace ParsingAndSaveTextInDb.Controllers
             _context = new ApplicationDbContext();
         }
 
+        private string[] SearchWord(string[] sentences, string word, string[] result)
+        {
+            int i = 0;
+            foreach (string item in sentences)
+            {
+                if (item.Contains(word))
+                {
+                    result[i] = item;
+                    i++;
+                }
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase upload, string word)
+        {
+            if (upload != null)
+            {
+                // получаєм ім'я файла
+                string fileName = System.IO.Path.GetFileName(upload.FileName);
+                // зберігаємо файл в папку Files в проекті
+                upload.SaveAs(Server.MapPath("~/Files/" + fileName));
+
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/Files/" + fileName));
+
+                string[] sentences = content.Split('.');
+                string[] result = new string[sentences.Length - 1];
+
+                result = SearchWord(sentences, word, result);
+
+                
+            }
+            return RedirectToAction("Index");
+        }
+
         // GET: File
         public ActionResult Index()
         {
